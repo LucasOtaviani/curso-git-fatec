b66097b2bfe8b86322f2127c8ac75ed1cec0c6ca
Lucas Otaviani <lucas.otaviani@hotmail.com>
2020-09-09 17:44:43 -0300

    Subindo o projeto

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PostsController
+{
+
+}
